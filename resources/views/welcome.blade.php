c14d4cee5814605f58c1fbbc11b1a817636bf3bd
ramzan07 <ramzan.umt@gmail.com>
2019-06-15 00:53:10 +0200

    row size changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -94,8 +94,9 @@
                     </div>
                 </div>
             </form>
+            <div class="container">
             <div class="row">
-                <div class="col-lg-8 col-md-10 mx-auto">
+                <div class="col-md-12">
                     @if(!empty($feed_posts))
                     @foreach($feed_posts as $item)
                     <div class="post-preview">
