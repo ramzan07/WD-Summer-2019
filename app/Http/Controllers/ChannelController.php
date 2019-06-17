a2810b9c7911b6c279e6dc111596fe62a788bf56
ramzan07 <ramzan.umt@gmail.com>
2019-06-17 02:09:45 +0200

    handler method for creating new channel and import feeds

--- app/Http/Controllers/ChannelController.php
+++ app/Http/Controllers/ChannelController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
+use DB;
 
 class ChannelController extends Controller {
 
@@ -35,39 +37,43 @@ class ChannelController extends Controller {
         $channel_data['channel_source']  = $request->input('url');
         $channel_data['channel_name']     = $request->input('name');
 
-
-        $create_channel = \App\RssChannel::firstOrCreate($channel_data);
-        if ($create_channel) {
+        $channel = \App\RssChannel::where('channel_source', '=', $request->input('url'))->first();
+        if(Input::get('save_only') && $channel === null) {
+            \App\RssChannel::firstOrCreate($channel_data);
             return redirect()->route('home')->with('success_message', 'Channel has been created successfully.');
+        } elseif(Input::get('save_import') && $channel === null){
+
+            $statement = DB::select("SHOW TABLE STATUS LIKE 'rss_channels'");
+            $nextChannelId = $statement[0]->Auto_increment;
+
+            $xmlStr  = file_get_contents($request->input('url'));
+            $xml     = simplexml_load_string($xmlStr, "SimpleXMLElement", LIBXML_NOCDATA);
+            $json    = json_encode($xml);
+            $array   = json_decode($json, TRUE);
+
+            if(isset($array['channel']['item'])) {
+                foreach ($array['channel']['item'] as $item) {
+                    $this->createPost($item, $nextChannelId);
+                }
+                \App\RssChannel::firstOrCreate($channel_data);
+                return redirect()->route('home')->with('success_message', 'Created successfully.');
+            } else {
+                return redirect()->back()->with('error_message', 'Broken Url');
+            }
+        } else {
+            return redirect()->back()->with('error_message', 'Chennel already Exists');
         }
-        //$xmlStr = file_get_contents($reques_params);
-        //$xml = simplexml_load_string($xmlStr, "SimpleXMLElement", LIBXML_NOCDATA);
-        //$json = json_encode($xml);
-        //$array = json_decode($json, TRUE);
-        //$channel = \App\RssChannel::where('channel_source', $array['channel']['link'])->first();
-        //if (empty($channel)) {
-          //  return $this->createNewChannel($array['channel']);
-        //$create_channel = \App\RssChannel::firstOrCreate($channel_array);
-        //}
-        //return redirect()->back()->with('error_message', 'Channel has already been created');
+
     }
 
-    /**
-     * createNewChannel method
-     * @param type $channel
-     * @return type
-     */
-    public function createNewChannel($channel) {
-       
-        $channel_array = [];
-        $channel_array['channel_name'] = $channel['title'];
-        $channel_array['channel_source'] = $channel['link'];
-        $channel_array['channel_description'] = $channel['title'];
-        $channel_array['channel_image'] =!empty( $channel['image'][0]['url']) ?  $channel['image'][0]['url'] : NULL;
-        $create_channel = \App\RssChannel::firstOrCreate($channel_array);
-        if ($create_channel) {
-            return redirect()->route('home')->with('Channel has been created successfully.');
-        }
+    public function createPost($item, $id) {
+
+        $rss['channel_id']  = $id;
+        $rss['title']       = $item['title'];
+        $rss['description'] = $item['description'];
+        $rss['link']        = $item['link'];
+        $rss['pubDate']     = date('Y-m-d H:i:s', strtotime($item['pubDate']));
+        \App\RssPost::create($rss);
     }
 
     /**
