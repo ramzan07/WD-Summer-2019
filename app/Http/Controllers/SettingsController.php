68e22f6cea71aade156be70de0f1833ef7f543e5
ramzan07 <ramzan.umt@gmail.com>
2019-06-20 08:48:00 +0200

    join with channels

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -3,43 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Input;
-Use DB;
 
-class ApiFeedController extends Controller {
-
-    use \App\Http\Traits\CommonService;
-
+class SettingsController extends Controller
+{
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index() {
-
-        $request_params = Input::all();
-
-        if (isset($request_params['id'])) {
-            $feeds = \App\RssPost::where('channel_id', $request_params['id'])->get();
-        } else {
-            $feeds = DB::table('rss_posts')->join('rss_channels', 'rss_channels.id', '=' , 'rss_posts.channel_id')
-            ->select('rss_posts.*')
-            ->where('rss_channels.status', 1)
-            ->get();
-
-        }
-
-        return $this->jsonSuccessResponse('Process is processed success', $feeds);
+    public function index()
+    {
+        return view('settings');
     }
 
     /**
-     * channels method 
-     * responsible for getting channels
+     * Show the form for creating a new resource.
+     *
      * @return \Illuminate\Http\Response
      */
-    public function channels() {
-        $channels = \App\RssChannel::all();
-        return $this->jsonSuccessResponse('Process is processed success', $channels);
+    public function create()
+    {
+        //
     }
 
     /**
@@ -48,7 +32,8 @@ class ApiFeedController extends Controller {
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
         //
     }
 
@@ -58,7 +43,8 @@ class ApiFeedController extends Controller {
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id) {
+    public function show($id)
+    {
         //
     }
 
@@ -68,7 +54,8 @@ class ApiFeedController extends Controller {
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id) {
+    public function edit($id)
+    {
         //
     }
 
@@ -79,7 +66,8 @@ class ApiFeedController extends Controller {
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id) {
+    public function update(Request $request, $id)
+    {
         //
     }
 
@@ -89,8 +77,8 @@ class ApiFeedController extends Controller {
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id) {
+    public function destroy($id)
+    {
         //
     }
-
 }
