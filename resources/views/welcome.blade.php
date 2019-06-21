fadc33ee074eb49b2e693a2c4d6ca1f309d3816c
ramzan07 <ramzan.umt@gmail.com>
2019-06-22 00:28:00 +0200

    js moved to external file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,106 +10,7 @@
 @endsection
 
 @section('page_scripts')
-<script type="text/javascript">
-    (function ($) {
-    $.fn.countTo = function (options) {
-        options = options || {};
-        
-        return $(this).each(function () {
-            // set options for current element
-            var settings = $.extend({}, $.fn.countTo.defaults, {
-                from:            $(this).data('from'),
-                to:              $(this).data('to'),
-                speed:           $(this).data('speed'),
-                refreshInterval: $(this).data('refresh-interval'),
-                decimals:        $(this).data('decimals')
-            }, options);
-            
-            // how many times to update the value, and how much to increment the value on each update
-            var loops = Math.ceil(settings.speed / settings.refreshInterval),
-                increment = (settings.to - settings.from) / loops;
-            
-            // references & variables that will change with each update
-            var self = this,
-                $self = $(this),
-                loopCount = 0,
-                value = settings.from,
-                data = $self.data('countTo') || {};
-            
-            $self.data('countTo', data);
-            
-            // if an existing interval can be found, clear it first
-            if (data.interval) {
-                clearInterval(data.interval);
-            }
-            data.interval = setInterval(updateTimer, settings.refreshInterval);
-            
-            // initialize the element with the starting value
-            render(value);
-            
-            function updateTimer() {
-                value += increment;
-                loopCount++;
-                
-                render(value);
-                
-                if (typeof(settings.onUpdate) == 'function') {
-                    settings.onUpdate.call(self, value);
-                }
-                
-                if (loopCount >= loops) {
-                    // remove the interval
-                    $self.removeData('countTo');
-                    clearInterval(data.interval);
-                    value = settings.to;
-                    
-                    if (typeof(settings.onComplete) == 'function') {
-                        settings.onComplete.call(self, value);
-                    }
-                }
-            }
-            
-            function render(value) {
-                var formattedValue = settings.formatter.call(self, value, settings);
-                $self.html(formattedValue);
-            }
-        });
-    };
-    
-    $.fn.countTo.defaults = {
-        from: 0,               // the number the element should start at
-        to: 0,                 // the number the element should end at
-        speed: 1000,           // how long it should take to count between the target numbers
-        refreshInterval: 100,  // how often the element should be updated
-        decimals: 0,           // the number of decimal places to show
-        formatter: formatter,  // handler for formatting the value before rendering
-        onUpdate: null,        // callback method for every time the element is updated
-        onComplete: null       // callback method for when the element finishes updating
-    };
-    
-    function formatter(value, settings) {
-        return value.toFixed(settings.decimals);
-    }
-}(jQuery));
-
-jQuery(function ($) {
-  // custom formatting example
-  $('.count-number').data('countToOptions', {
-    formatter: function (value, options) {
-      return value.toFixed(options.decimals).replace(/\B(?=(?:\d{3})+(?!\d))/g, ',');
-    }
-  });
-  
-  // start all the timers
-  $('.timer').each(count);  
-  
-  function count(options) {
-    var $this = $(this);
-    options = $.extend({}, options || {}, $this.data('countToOptions') || {});
-    $this.countTo(options);
-  }
-});
-</script>
+<script src="{{asset('public/js/counter.js')}}"></script>
 
 <script type="text/javascript">
     function loadDatbaseDetails(id)
@@ -136,6 +37,23 @@ jQuery(function ($) {
     });
 </script>>
 
+@endsection
+
+@section('sidebarCards')
+<div class="my-detail col"  style="margin-bottom: 0px;">
+    <div class="white-spacing">
+      <i class="fa fa-code fa-2x"></i>
+      <h1 class="timer count-title count-number" data-to="100" data-speed="1500"></h2>
+      <h1>News Count</h1>
+    </div>
+      </div>
+<div class="my-detail col"  style="margin-bottom: 0px;">
+    <div class="white-spacing">
+      <i class="fa fa-code fa-2x"></i>
+      <h1 class="timer count-title count-number" data-to="200" data-speed="1500"></h2>
+      <h1>News Count</h1>
+    </div>
+</div>
 @endsection
 
 @section('page_heading')
