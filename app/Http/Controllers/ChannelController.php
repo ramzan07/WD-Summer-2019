89de5e1969d3f6a917ab6327f43dd87ce0624070
ramzan07 <ramzan.umt@gmail.com>
2019-06-21 15:08:32 +0200

    providers page created

--- app/Http/Controllers/ChannelController.php
+++ app/Http/Controllers/ChannelController.php
@@ -14,7 +14,7 @@ class ChannelController extends Controller {
      * @return \Illuminate\Http\Response
      */
     public function index() {
-        
+        return view('providers');
     }
 
     /**
